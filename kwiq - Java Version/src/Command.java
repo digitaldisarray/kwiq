fa0f1ae896a160e295280a31f4cfd6ac43f14482
Kevin <digitaldisarray@users.noreply.github.com>
2019-03-19 14:54:20 -0700

    Update Command.java

--- kwiq - Java Version/src/Command.java
+++ kwiq - Java Version/src/Command.java
@@ -7,48 +7,11 @@ public class Command {
 	private String syntax;
 	private ArrayList<Object> args;
 
-	/**
-	 * Creates a command object with only a name.
-	 * 
-	 * Default Values Assigned:
-	 *  - Description: blank
-	 *  - Syntax: "No syntax specified"
-	 *  - Args: Empty ArrayList of class: Object
-	 * 
-	 * @param name - The name of the command.
-	 */
+	
 	public Command(String name) {
 		this(name, "", "No syntax specified.", new ArrayList<>());
 	}
-
-	/**
-	 * Creates a command object with only a name and description.
-	 * 
-	 * Default Values Assigned:
-	 *  - Syntax: "No syntax specified"
-	 *  - Args: Empty ArrayList of class: Object
-	 * 
-	 * @param name - The name of the command.
-	 * @param description - The description of what the command will do.
-	 */
-	public Command(String name, String description) {
-		this(name, description, "No syntax specified.", new ArrayList<>());
-	}
-
-	/**
-	 * Creates a command object with only a name, description, and syntax.
-	 * 
-	 * Default Values Assigned:
-	 *  - Args: Empty ArrayList of class: Object
-	 * 
-	 * @param name - The name of the command.
-	 * @param description - The description of what the command will do.
-	 * @param syntax - The commands usage/syntax. Typically includes an example command or two.
-	 */
-	public Command(String name, String description, String syntax) {
-		this(name, description, syntax, new ArrayList<>());
-	}
-
+	
 	/**
 	 * Creates a command object with only a name, description, syntax, and argument types.
 	 * 
